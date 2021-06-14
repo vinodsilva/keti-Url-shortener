4d23d2aee5dcede8ec0dabae1d6f43c3ca1d9e4c
vinod <vinodsilva.10@gmail.com>
2021-06-14 20:27:44 +0530

    updated route slug

--- controller/routes.js
+++ controller/routes.js
@@ -150,7 +150,7 @@ router.get('/:slug?',async (req, res) => {
     if (req.params.slug != undefined) {
         var data = await urls.findOne({ slug: req.params.slug });
         if (data) {
-            res.render("veiwUrl/:slug", {data, csrfToken: req.csrfToken() });
+            res.render("veiwUrl", {data, csrfToken: req.csrfToken() });
      }
 
 
@@ -167,7 +167,7 @@ router.get('/:slug?',async (req, res) => {
 router.post('/slug', async (req, res) => {
 const { password } = req.body;
 if (!password ) {
-    res.render("veiwUrl/:slug", { err: "All Fields Required !", csrfToken: req.csrfToken() });
+    res.render("veiwUrl", { err: "All Fields Required !", csrfToken: req.csrfToken() });
 } 
  else {
     var data = await urls.findOne({ slug: req.params.slug });
