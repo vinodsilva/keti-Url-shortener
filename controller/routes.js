22a4daca4a727a5e387a639fa3daa4d99acc5edb
vinod <vinodsilva.10@gmail.com>
2021-06-14 21:31:56 +0530

    url post

--- controller/routes.js
+++ controller/routes.js
@@ -164,44 +164,44 @@ router.get('/:slug?',async (req, res) => {
 
 });
 
-// router.post('/slug', async (req, res) => {
-// const { password } = req.body;
-// if (!password ) {
-//     res.render("veiwUrl", { err: "All Fields Required !", csrfToken: req.csrfToken() });
-// } 
-//  else {
-//     var data = await urls.findOne({ slug: req.params.slug });
-//     if (data) {
-//                 data.visits = data.visits + 1;
+router.post('/slug', async (req, res) => {
+const { password } = req.body;
+if (!password ) {
+    res.render("veiwUrl", { err: "All Fields Required !", csrfToken: req.csrfToken() });
+} 
+ else {
+    var data = await urls.findOne({ slug: req.params.slug });
+    if (data) {
+                data.visits = data.visits + 1;
     
-//                 var ref = req.query.ref;
-//                 if (ref) {
-//                     switch (ref) {
-//                         case 'fb':
-//                             data.visitsFB = data.visitsFB + 1;
-//                             break;
-//                         case 'ig':
-//                             data.visitsIG = data.visitsIG + 1;
-//                             break;
-//                         case 'yt':
-//                             data.visitsYT = data.visitsYT + 1;
-//                             break;
-//                     }
-//                 }
+                var ref = req.query.ref;
+                if (ref) {
+                    switch (ref) {
+                        case 'fb':
+                            data.visitsFB = data.visitsFB + 1;
+                            break;
+                        case 'ig':
+                            data.visitsIG = data.visitsIG + 1;
+                            break;
+                        case 'yt':
+                            data.visitsYT = data.visitsYT + 1;
+                            break;
+                    }
+                }
     
-//                 await data.save();
+                await data.save();
                
-//              res.redirect(data.originalUrl);
+             res.redirect(data.originalUrl);
             
-//             } else {
-//                 if (req.isAuthenticated()) {
-//                     res.render("index", { logged: true, err: true });
-//                 } else {
-//                     res.render("index", { logged: false, err: true });
-//                 }
+            } else {
+                if (req.isAuthenticated()) {
+                    res.render("index", { logged: true, err: true });
+                } else {
+                    res.render("index", { logged: false, err: true });
+                }
     
-//             }
-// }
+            }
+}
 
 
 
