1b804e84019b1cc2a84461df4755c17fa7048ec3
vinod silva <66962737+vinodsilva@users.noreply.github.com>
2021-08-06 10:31:37 +0530

    Update googleData.js
    removed google secret key

--- config/googleData.js
+++ config/googleData.js
@@ -1,2 +1,2 @@
-module.exports.clientId = "<Add Your Client ID>";
-module.exports.clientSecret = "AIzaSyDZlL7m1mSHpC4-K1vR7ODaShhm63opM7Y";
+module.exports.clientId = "";
+module.exports.clientSecret = "";
